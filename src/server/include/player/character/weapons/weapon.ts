929eaf283af13c2643dcd249e866b3c4f8312b93
Sebastian Waldbauer <sebi.developer@gmail.com>
2019-10-29 19:27:51 +0100

    Start with weapon system
    Signed-off-by: Sebastian Waldbauer <sebi.developer@gmail.com>

--- src/server/include/player/character/weapons/weapon.ts
+++ src/server/include/player/character/weapons/weapon.ts
@@ -2,9 +2,11 @@ import { Meele, Handguns } from "./type";
 
 export class Weapon {
     private _id: number; // TODO: set internal weapon number, so cheat is not possible!
-    private _w: Meele | Handguns;
-    constructor(weapon:Meele | Handguns) {
+    private _weapon: Meele | Handguns;
+    private _ammo: number;
+    constructor(weapon:Meele | Handguns, ammo:number) {
         this._id = 0; // TODO: generate random weapon id for anti cheat :)
-        this._w = weapon;
+        this._weapon = weapon;
+        this._ammo = ammo; // TODO: Client send shoot event to calc new server-side ammo.
     }
 }
