d7c25a2c66f8213c1ad821a8cdfcf78f5e7ae616
Sebastian Waldbauer <sebi.developer@gmail.com>
2019-10-30 17:23:11 +0100

    Added key/value pair player additions
    Signed-off-by: Sebastian Waldbauer <sebi.developer@gmail.com>

--- src/server/include/player/index.ts
+++ src/server/include/player/index.ts
@@ -1,20 +1,35 @@
 import { Player } from "alt";
 import { Weapon } from "./character/weapons/weapon";
+import { Sync } from "../sync";
 
 export class FWPlayer {
     _player: Player;
+    CustomSync: Sync;
     IpAddress: string;
     Weapons: Map<number, Weapon>;
+    private _kv: Map<string, any>;
 
     constructor(player:Player) {
         this._player = player;
         this.IpAddress = this._player.ip;
         this.Weapons = new Map<number, Weapon>();
-
-        //this.Weapons.push(new Weapon(Meele.Machete));
+        this.CustomSync = new Sync();
+        this._kv = new Map<string, any>();
     }
 
     getNativePlayer():Player {
         return this._player;
+    }
+
+    set(key:string, value:any):void {
+        this._kv.set(key, value);
+    }
+
+    get(key:string):any {
+        return this.get(key);
+    }
+
+    has(key:string):boolean {
+        return this._kv.has(key);
     }
 }
