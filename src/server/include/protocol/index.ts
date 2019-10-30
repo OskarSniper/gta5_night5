68bff1d65a3753d135e1b5bb4a8d2d48f2359fe6
Sebastian Waldbauer <sebi.developer@gmail.com>
2019-10-30 12:56:39 +0100

    register and unregister packages
    Signed-off-by: Sebastian Waldbauer <sebi.developer@gmail.com>

--- src/server/include/protocol/index.ts
+++ src/server/include/protocol/index.ts
@@ -1,18 +1,34 @@
 export class Protocol {
     private _scappedEvents: Map<string, string>;
+    private _eventToPackage: Map<string, any>;
     private _countEvents: number;
 
     constructor() {
         this._scappedEvents = new Map<string, string>();
+        this._eventToPackage = new Map<string, any>();
         this._countEvents = 0;
     }
 
-    registerPackage(origin:string):void {
+    registerPackage(origin:string):boolean {
         if(!this._scappedEvents.has(origin)) {
             this._countEvents = this._countEvents + 1;
             this._scappedEvents.set(origin, this._generateId(this._countEvents));
+            console.log(`${origin} successfully registered!`);
+            return true;
         } else {
             console.log(`Conflict! ${origin} is already registered!`);
+            return false;
+        }
+    }
+
+    unregisterPackage(origin:string):boolean {
+        if(this._scappedEvents.has(origin)) {
+            this._scappedEvents.delete(origin);
+            console.log(`Package ${origin} successfully unregistered!`);
+            return true;
+        } else {
+            console.log(`Conflict! ${origin} not registered, no action taken!`);
+            return false;
         }
     }
 
