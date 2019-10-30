1ca6e2cf48aa7e03483a8a9868ca8281c7398d6d
Sebastian Waldbauer <sebi.developer@gmail.com>
2019-10-30 15:30:44 +0100

    Network packaging & computing is now done automatically
    Signed-off-by: Sebastian Waldbauer <sebi.developer@gmail.com>

--- src/server/include/protocol/index.ts
+++ src/server/include/protocol/index.ts
@@ -1,18 +1,51 @@
+/**
+ * Protocol class for a smoother networking & fix all related
+ * network issues. Built for high performance networks & clustering.
+ * @author OskarSniper <Sebastian Waldbauer>
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 export class Protocol {
+    /**
+     * scappedEvents contains the original package name and the shorten one.
+     * This has been made to reduce network traffic & save bandwith.
+     */
     private _scappedEvents: Map<string, string>;
+
+    /**
+     * Is used for Network2Package, so we can directly initialize a new incoming package
+     * and call them natively.
+     */
     private _eventToPackage: Map<string, any>;
+
+    /**
+     * Counts upwards to guarantee no duplicates in network identifiers.
+     */
     private _countEvents: number;
 
+    /**
+     * Initialize all variables
+     * @constructor
+     */
     constructor() {
         this._scappedEvents = new Map<string, string>();
         this._eventToPackage = new Map<string, any>();
         this._countEvents = 0;
     }
 
-    registerPackage(origin:string):boolean {
+    /**
+     * Registers a new network package to the whole system. This is used to detect & maybe
+     * optimize bottlenecks in further network related stuff.
+     * @param {string} origin
+     * @param {any} pack
+     * @returns {boolean}
+     */
+    registerPackage(origin:string, pack:any):boolean {
         if(!this._scappedEvents.has(origin)) {
             this._countEvents = this._countEvents + 1;
-            this._scappedEvents.set(origin, this._generateId(this._countEvents));
+            let id:string = this._computeNewNetworkId(this._countEvents);
+            this._scappedEvents.set(origin, id);
+            this._eventToPackage.set(id, pack);
             console.log(`${origin} successfully registered!`);
             return true;
         } else {
@@ -21,8 +54,17 @@ export class Protocol {
         }
     }
 
+    /**
+     * Used for removing network packages, maybe save some RAM, but shouldnt be used
+     * that often.
+     * @param {string} origin
+     * @returns {boolean}
+     */
     unregisterPackage(origin:string):boolean {
         if(this._scappedEvents.has(origin)) {
+            if(this._eventToPackage.has(this._scappedEvents.get(origin) as string)) {
+                this._eventToPackage.delete(this._scappedEvents.get(origin) as string);
+            }
             this._scappedEvents.delete(origin);
             console.log(`Package ${origin} successfully unregistered!`);
             return true;
@@ -32,14 +74,19 @@ export class Protocol {
         }
     }
 
-    private _generateId(input:number):string {
+    /**
+     * Computing a new network identifier for network packages.
+     * @param {number} c
+     * @returns {string}
+     */
+    private _computeNewNetworkId(c:number):string {
         let id:string = "";
-        let alphabet:string = "0123456789abcdef";
+        let alphabet:string = "0123456789abcdefghijklmnopqrstuvwxyz";
         let alphabetLength:number = alphabet.length;
         do {
-            id = alphabet[input % alphabetLength] + id;
-            input = parseInt((input / alphabetLength).toString(), 10);
-        } while (input);
+            id = alphabet[c % alphabetLength] + id;
+            c = parseInt((c / alphabetLength).toString(), 10);
+        } while (c);
         return id;
     }
 }
