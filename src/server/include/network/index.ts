07fc35ec1714ecefb371cde7be2f9439b87a8b84
Sebastian Waldbauer <sebi.developer@gmail.com>
2019-11-01 02:12:16 +0100

    Fixed some in networking
    Signed-off-by: Sebastian Waldbauer <sebi.developer@gmail.com>

--- src/server/include/network/index.ts
+++ src/server/include/network/index.ts
@@ -1,13 +1,21 @@
 import { Protocol } from "./protocol";
 import { Event } from "./events";
-import { Player, emitClient } from "alt";
+import alt, { Player } from "alt";
+import { EventEmitter } from "events";
 
 export class Network {
     Protocol: Protocol;
     Event: Event;
+    private _em: EventEmitter;
+
     constructor() {
         this.Event = new Event();
         this.Protocol = new Protocol();
+        this._em = new EventEmitter();
+    }
+
+    _getProtocolEmitter():EventEmitter {
+        return this._em;
     }
 
     emit(player:Player|null, ev:string, ...data:any):void {
@@ -16,12 +24,19 @@ export class Network {
         // added fallback support to native packaging
         if(this.Protocol.Events().has(ev)) {
             event = this.Protocol.Events().get(ev) as string;
+        } else {
+            console.log("Using native packaging... Not recommended!");
         }
-        emitClient(player, event, data);
+
+        alt.emitClient(player, event, data);
     }
 
+    on(event:string, cb:any):void {
+        this.Event.getEventEmitter().on(event, cb);
+    }
+
+    /*
     on(ev:string, cb:any):void {
-        console.log("New protocol message received!" + ev);
         let event:string = ev;
         let pack:any = {};
 
@@ -31,8 +46,11 @@ export class Network {
             if(this.Protocol.Packages().has(event)) {
                 pack = this.Protocol.Packages().get(event);
             }
+        } else {
+            console.log("Using native packaging... Not recommended!");
         }
 
         console.log(pack);
     }
+    */
 }
